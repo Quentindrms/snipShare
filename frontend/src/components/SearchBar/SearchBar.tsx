5edd89ef760ca732a86324e0e66503345df7d5a9
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-28 14:36:00 +0100

    Création de composants pour affichage des snippets avec leur nom et description Création d'un composant searchBar pour rechercher les snippets Ajout d'un bouton sur la page browseSnippet pour accèder à une recherche avancée

---
+++ frontend/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,13 @@
+import './searchbar.css';
+
+
+export function SearchBar(){
+
+    return(
+        <>
+            <input  className='searchbar' type='text' placeholder='Rechercher'></input>
+        </>
+    )
+}
+
+export default SearchBar;
--- frontend/src/views/BrowseSnippets.tsx
+++ frontend/src/views/BrowseSnippets.tsx
@@ -1,10 +1,23 @@
-
+import PrimaryButton from "../components/Buttons/PrimaryButton";
+import SearchBar from "../components/SearchBar/SearchBar";
+import SnippetArea from "../components/SnippetArea/SnippetArea";
 
 export function BrowseSnippets(){
 
     return (
         <>
-            <p>Browse snippets</p>
+            <h2 className="title-2-centered">Parcourir les snippets</h2>
+            <div className="browsepage-cta">
+                <SearchBar />
+                <PrimaryButton buttonText="Recherche avancée" buttonLink="#" />
+            </div>
+            <div className="snippetsContainer">
+                <SnippetArea snippetTitle="Lorem ipsum"
+                snippetSummary="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas odio lorem, maximus vel ipsum eu, venenatis dictum tellus. Pellentesque ut mauris malesuada lectus dictum placerat a quis risus. Aenean ut gravida tellus. Mauris porta metus a venenatis consequat. Morbi tortor sem, interdum a imperdiet ut, iaculis id nisl. Vestibulum ut convallis metus."
+                snippetNumberComments={10}
+                snippetNumberLikes={10}
+                 />
+            </div>
         </>
     )
 }
