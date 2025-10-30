67ec6d2e6aa32cb2bb4b572999c20ac0d9e6ea6b
Quentin Derimais <quentin.derimais@gmail.com>
2025-10-30 14:54:07 +0100

    Mise en place du controller Snippet des routes associées Ajout du dtabase_schema pour création base de données Ajout d'un .env.example pour variable d'environnement avec connexion base de données

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import snippetRouter from "./Snippet";
 
 const router = Router();
 
 router.get('/', (request, response) => {
     response.send('Backend fonctionnel');
 })
+
+router.use('/snippets', snippetRouter);
 
 export default router;
